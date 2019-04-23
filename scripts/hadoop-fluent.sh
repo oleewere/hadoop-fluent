6930e4bb94e8729cb8c946291d815afb76b92d84
Oliver Szabo <oleewere@gmail.com>
2019-04-23 06:09:49 +0200

    make start script work

--- scripts/hadoop-fluent.sh
+++ scripts/hadoop-fluent.sh
@@ -4,10 +4,41 @@ function print_help() {
   cat << EOF
    Usage: hadoop-fluent.sh --type <STORAGE_TYPE> --path <PATH> [additional options]
 
-   -t, --type  <STORAGE_TYPE>                      available storage types: s3 | gcs | wasb | adlsv2 | hdfs
-   -c, --core-site-folder <CORE_SITE_FOLDER_PATH>  core-site.xml path
+   -t, --type  <STORAGE_TYPE>                      available storage types: s3 | gcs | wasb | abfs
+   -s, --core-site-folder <CORE_SITE_FOLDER_PATH>  core-site.xml path
    -h, --help                                      print help
 EOF
+}
+
+readlinkf(){
+  # get real path on mac OSX
+  perl -MCwd -e 'print Cwd::abs_path shift' "$1";
+}
+
+if [ "$(uname -s)" = 'Linux' ]; then
+  SCRIPT_DIR="`dirname "$(readlink -f "$0")"`"
+else
+  SCRIPT_DIR="`dirname "$(readlinkf "$0")"`"
+fi
+
+HADOOP_FLUENT_ROOT_DIR="`dirname \"$SCRIPT_DIR\"`"
+
+JVM="java"
+
+if [ -x $JAVA_HOME/bin/java ]; then
+  JVM=$JAVA_HOME/bin/java
+fi
+
+if [[ -z "$HADOOP_FLUENT_CONF" ]]; then
+  HADOOP_FLUENT_CONF="$HADOOP_FLUENT_ROOT_DIR/conf/hadoop-fluent.conf"
+fi
+
+function run_app() {
+  local sdk_type=$1
+  local core_site_folder=$2
+
+  $JVM -classpath "$HADOOP_FLUENT_ROOT_DIR/libs/core/*:/$HADOOP_FLUENT_ROOT_DIR/libs/$sdk_type/*:$core_site_folder" \
+    com.cloudera.hadoop.cloud.Forward "$HADOOP_FLUENT_CONF"
 }
 
 function main() {
@@ -44,6 +75,19 @@ function main() {
   if [[ -z "$CORE_SITE_FOLDER" ]] ; then
     echo "path argument is required (-c or --core-site-folder)."
     print_help
+    exit 1
+  fi
+
+  if [[ "$STORAGE_TYPE" == "s3" ]]; then
+    run_app "s3" "$CORE_SITE_FOLDER"
+  elif [[ "$STORAGE_TYPE" == "gcs" ]]; then
+    run_app "gcs" "$CORE_SITE_FOLDER"
+  elif [[ "$STORAGE_TYPE" == "abfs" ]]; then
+    run_app "abfs" "$CORE_SITE_FOLDER"
+  elif [[ "$STORAGE_TYPE" == "wasb" ]]; then
+    run_app "wasb" "$CORE_SITE_FOLDER"
+  else
+    echo "Unsupported type:. $STORAGE_TYPE"
     exit 1
   fi
 }
